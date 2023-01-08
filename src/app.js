4c8f45ec855f691a39898d2ba6bd7264e97b0ad6
agusalvs <agustina.alvarezsacra@gmail.com>
2023-01-08 00:04:07 +0000

    Payment form last version

--- src/app.js
+++ src/app.js
@@ -8,9 +8,62 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   let form = document.querySelector("form");
   form.addEventListener("submit", enviarDatos);
+  // form.addEventListener("submit", ccVal);
+
+  //función para validar tarjeta de crédito
+  //fuente: https://www.w3schools.blog/credit-card-validation-javascript-js
+
+  // function ccVal(cardNumber) {
+  //   console.log(cardNumber);
+  //   let regexMasterCard = /^5[1-5][0-9]{14}$|^2(?:2(?:2[1-9]|[3-9][0-9])|[3-6][0-9][0-9]|7(?:[01][0-9]|20))[0-9]{12}$/;
+  //   let regexAmex = /^3[47][0-9]{13}$/;
+  //   let regexVisa = /^4[0-9]{12}(?:[0-9]{3})?$/;
+  //   let regexMaestro = /^(5018|5081|5044|5020|5038|603845|6304|6759|676[1-3]|6799|6220|504834|504817|504645)[0-9]{8,15}$/;
+
+  //   if (
+  //     cardNumber.value.match(
+  //       /^5[1-5][0-9]{14}$|^2(?:2(?:2[1-9]|[3-9][0-9])|[3-6][0-9][0-9]|7(?:[01][0-9]|20))[0-9]{12}$/
+  //     ) ||
+  //     cardNumber.value.match(regexAmex) ||
+  //     cardNumber.value.match(regexVisa) ||
+  //     cardNumber.value.match(regexMaestro)
+  //   ) {
+  //     return true;
+  //   } else {
+  //     alert("Please enter a valid credit card number.");
+  //     return false;
+  //   }
+  // }
+
+  // función para lograr que el valor del credit card # sea solo numerico
+  // fuente: https://bbbootstrap.com/snippets/credit-card-checkout-formatted-input-51353295
+
+  //For Card Number formatted input
+  let cardNum = document.getElementById("cardNumber");
+  cardNum.onkeyup = function(e) {
+    if (this.value == this.lastValue) return;
+    let caretPosition = this.selectionStart;
+    let sanitizedValue = this.value.replace(/[^0-9]/gi, "");
+    let parts = [];
+
+    for (let i = 0, len = sanitizedValue.length; i < len; i += 4) {
+      parts.push(sanitizedValue.substring(i, i + 4));
+    }
+
+    for (let i = caretPosition - 1; i >= 0; i--) {
+      let c = this.value[i];
+      if (c < "0" || c > "9") {
+        caretPosition--;
+      }
+    }
+    caretPosition += Math.floor(caretPosition / 4);
+
+    this.value = this.lastValue = parts.join(" ");
+    this.selectionStart = this.selectionEnd = caretPosition;
+  };
 
   function enviarDatos(e) {
-    e.preventDefault(); // detiene tu comportamiento predeterminado del formulario e interpretado mi codigo
+    e.preventDefault(); // detiene tu comportamiento predeterminado del formulario e interpreta mi codigo
 
     //obtengo el elemento por el id, obtengo el elemento de la alerta y devuelvo el valor del elemento.
     let card = document.getElementById("cardNumber");
@@ -32,7 +85,7 @@ window.onload = function() {
     console.log(zip.value);
     let radios = document.getElementById("radios");
     console.log(radios.value);
-    let msgLabel = document.getElementById("accept");
+    let accept = document.getElementById("accept");
     console.log(accept.value);
     let message = document.getElementById("message");
     console.log(message.value);
@@ -75,5 +128,5 @@ window.onload = function() {
       message.classList.add("bg-danger", "bg-opacity-25");
     }
   }
-  console.log("Hello Rigo from the console!");
+  console.log("Hello TA!");
 };
